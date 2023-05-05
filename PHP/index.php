f1a20946fe115bff7480af2aeb6b68ad2c5d68d5
Frank Anthony Napoles <79691793+idlechamp2000@users.noreply.github.com>
2023-05-05 21:01:09 +0800

    Add files via upload

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,8 @@
-<?php
-    include("startsession.php");
+<?php 
+    include 'PHP/startsession.php';
+    if(isset($_POST['submit'])){
+        header('location: PHP/products_medpage.php');
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles_loginnavie.css">
     <title> Almed Marikina </title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,9 +18,9 @@
 </head>
 <body>
     <!--<div class="banner">-->
-        <?php include 'navbar.php';?>
+        <?php include 'PHP/navbar.php';?>
         <div id="titlecard">
-            <h1>Welcome to ALMed Marikina</h1>
+            <img src="images/logurr_effec2.png" id="logo-site">
             <p>Accessible local medicine from local pharmacies of Marikina</p>
         </div>
         <div id="maincard">
@@ -24,7 +28,7 @@
                 <h2 class="title">An Online Portal for Viewing Medicines</h2>
                 <p class="description">The website allows you to check the availability of medicines from pharmacies
                 in Marikina City</p>
-                <img src="images\sampleimg.jpg">
+                <img src="images/Online%20Pharmacy%20set1.jpg">
             </div>
         </div>
             <div class="category-card">
@@ -34,21 +38,21 @@
                         availability</p>
                 </div>
                 <div class="category-flex">
-                    <div class="category-unit">
+                    <a href = "../PHP/products_medpage.php?category_id=1" class="category-unit">
                         <h3>Analgesics</h3>
-                    </div>
-                    <div class="category-unit">
-                        <h3>Colds, cough, and flu</h3>
-                    </div>
-                    <div class="category-unit">
-                        <h3>First Aid</h3>
-                    </div>
-                    <div class="category-unit">
-                        <h3>Vitamins</h3>
-                    </div>
+                    </a>
+                    <a href = "../PHP/products_medpage.php?category_id=2" class="category-unit">
+                        <h3>Antibiotics</h3>
+                    </a>
+                    <a href = "../PHP/products_medpage.php?category_id=3" class="category-unit">
+                        <h3>Antihistamine</h3>
+                    </a>
+                    <a href = "../PHP/products_medpage.php?category_id=4" class="category-unit">
+                        <h3>Decongestants</h3>
+                    </a>
                 </div>
                 <div class="button">
-                    <a href="products.php" class="view-all">View all categories</a>
+                    <a href="PHP/products.php#shop_category" class="view-all">View all categories</a>
                 </div>
             </div>
             <div class="card-diff-color">
@@ -58,36 +62,29 @@
                         local, family-owned pharmacies from Marikina City. This ensures
                         that your medicine comes from real pharmacies, from Marikina too!
                     </p>
-                    <img src="images\samplephoto.jpg" id="own-photo">
+                    <img src="images/Online%20Pharmacy%20set2.jpg" id="own-photo">
                 </div>
                 <div class="button">
-                    <a href="products.php" class="view-all">View all pharmacies</a>
+                    <a href="PHP/products.php#shop_pharmacy" class="view-all">View all pharmacies</a>
                 </div>
             </div>
         <div id="footercard">
             <div class="footer-sub">
                 <h4>ALMed Marikina</h4>
-                <a href="#">Terms and Condition</a>
-                <a href="#">Privacy Policy</a>
+                <a href="Terms.html">Terms and Condition</a>
+                <a href="Privacy.html">Privacy Policy</a>
             </div>
             <div class="footer-sub">
                 <h5>About Us</h5>
-                <a href="index2.html">About the Website</a>
-                <a href="#">Contact Us</a>
+                <a href="PHP/about.php">About the Website</a>
+                <a href="PHP/contact.php#contact">Contact Us</a>
             </div>
             <div class="footer-sub">
                 <h5>Shop</h5>
-                <a href="products.php">Available Medicine</a>
-                <a href="products.php">Partner Pharmacies</a>
+                <a href="PHP/products.php#shop_category">Available Medicine</a>
+                <a href="PHP/products.php#shop_pharmacy">Partner Pharmacies</a>
             </div>
         </div>
-        <!--<div class="content">
-            <h1>Welcome to Almed Marikina</h1>
-                <p></p>
-                <button type="button"><span></span> Click here</button>
-                <button type="button"><span></span> Click here</button>
-        </div>-->
-    <!--</div>-->
 </body>
 </html>
 
